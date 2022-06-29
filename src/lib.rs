f5816ee7606185c51c08738c638dbc09196be0f9
Jeremy Fitzhardinge <jeremy@goop.org>
2022-06-28 22:51:20 -0700

    update docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,12 @@
 //! This crate implements splitting, joining and indexing tuples.
 //!
 //! The traits are implemented for tuples from zero len (ie, `()` unit) to 16.
+//! (More with with the `tuple_24` and `tuple_32` features enabled.)
 //!
+//! They are implenented for both tuples by value and references, which either
+//! consume or borrow their inputs respectively.
+//!
+//! An example of [`TupleJoin`] by value:
 //! ```rust
 //! use tuplestructops::TupleJoin;
 //!
@@ -20,19 +25,19 @@
 //! println!("out {out:?}");
 //! ```
 //!
-//! [`TupleIdx`] allows a single tuple member to be referenced. `idx` gets a
-//! reference to a field, and `extract` moves it out.
+//! [`TupleIdx`] allows a single tuple member to be referenced with `idx`.
 use seq_macro::seq;
 
 /// Implement `join` for tuples.
 ///
 /// `Self` is the left side of the join, and right is the `RHS` type parameter.
 pub trait TupleJoin<RHS>: seal::Sealed {
-    /// Joined output type.
+    /// Joined output type. See [`TupleJoinOutput`] as an easy way to reference
+    /// this in other type contexts.
     type Output;
 
-    /// Join two tuples, consuming both. `self` is the left (prefix) and `other`
-    /// is the right (suffix).
+    /// Join two tuples. `self` is the left (prefix) and `other` is the right
+    /// (suffix).
     /// ```rust
     /// # use tuplestructops::TupleJoin;
     /// let out: (_, _, _, _, _, _) = (1, 2, 3).join(('a', 'b', 'c'));
@@ -45,7 +50,18 @@ pub trait TupleJoin<RHS>: seal::Sealed {
     fn join(self, other: RHS) -> Self::Output;
 }
 
-/// Resulting type of joining tuples `L` and `R`.
+/// Resulting type of joining tuples `L` and `R`. This is useful for referencing it in other type contexts. For example:
+/// ```rust
+/// # use std::path::PathBuf;
+/// # use tuplestructops::{TupleJoin, TupleJoinOutput};
+/// type DefaultBits = (String, PathBuf);
+///
+/// fn add_default<E>(extra: E) -> TupleJoinOutput<DefaultBits, E>
+/// where DefaultBits: TupleJoin<E>
+/// {
+///     ("hello".to_string(), PathBuf::from("/world")).join(extra)
+/// }
+/// ```
 pub type TupleJoinOutput<L, R> = <L as TupleJoin<R>>::Output;
 
 /// Split a tuple into left and right portions.
@@ -76,6 +92,11 @@ pub trait TupleIdx<const N: usize>: seal::Sealed {
 }
 
 /// Helper for [`TupleIdx`] which makes it a bit easier to express.
+///
+/// ```rust
+/// # use tuplestructops::tuple_idx;
+/// let a: char = tuple_idx::<1, _>((1, 'a', 2.3));
+/// ```
 pub fn tuple_idx<const N: usize, T: TupleIdx<N>>(tup: T) -> T::Output {
     tup.idx()
 }
