3f0edf1f00315b819628bd153273d2283d2fb841
Jeremy Fitzhardinge <jeremy@goop.org>
2022-06-28 23:26:13 -0700

    restructures sources

---
+++ src/macro_impl.rs
@@ -0,0 +1,116 @@
+use super::*;
+
+macro_rules! impl_tupleops {
+    (@impl $($left:ident)* ; $($right:ident)*) => {
+        // Join by value
+        #[cfg_attr(not(feature = "impl_docs"), doc(hidden))]
+        impl<$($left,)* $($right,)*> TupleJoin<($($right,)*)> for ($($left,)*) {
+            type Output = ($($left,)* $($right,)*);
+
+            #[allow(clippy::unused_unit, non_snake_case)]
+            fn join(self, other: ($($right,)*)) -> Self::Output {
+                let ($($left,)*) = self;
+                let ($($right,)*) = other;
+
+                ($($left,)* $($right,)*)
+            }
+        }
+
+        // Join by reference
+        #[cfg_attr(not(feature = "impl_docs"), doc(hidden))]
+        impl<'a, $($left,)* $($right,)*> TupleJoin<&'a ($($right,)*)> for &'a ($($left,)*) {
+            type Output = ($(&'a $left,)* $(&'a $right,)*);
+
+            #[allow(clippy::unused_unit, non_snake_case)]
+            fn join(self, other: &'a ($($right,)*)) -> Self::Output {
+                let ($($left,)*) = self;
+                let ($($right,)*) = other;
+
+                ($($left,)* $($right,)*)
+            }
+        }
+
+        // Split by value
+        #[cfg_attr(not(feature = "impl_docs"), doc(hidden))]
+        impl<$($left,)* $($right,)*> TupleSplit<($($left,)*), ($($right,)*)> for ($($left,)* $($right,)*) {
+            #[allow(clippy::unused_unit, non_snake_case)]
+            fn split(self) -> (($($left,)*), ($($right,)*)) {
+                let ($($left,)* $($right,)*) = self;
+
+                (($($left,)*), ($($right,)*))
+            }
+        }
+
+        // Split by reference
+        #[cfg_attr(not(feature = "impl_docs"), doc(hidden))]
+        impl<'a, $($left,)* $($right,)*> TupleSplit<($(&'a $left,)*), ($(&'a $right,)*)> for &'a ($($left,)* $($right,)*) {
+            #[allow(clippy::unused_unit, non_snake_case)]
+            fn split(self) -> (($(&'a $left,)*), ($(&'a $right,)*)) {
+                let ($($left,)* $($right,)*) = self;
+
+                (($($left,)*), ($($right,)*))
+            }
+        }
+    };
+    (@recur $($left:ident)* ; ) => {
+        impl_tupleops!(@impl $($left)* ; );
+    };
+    (@recur $($left:ident)* ; $first:ident $($rest:ident)*) => {
+        impl_tupleops!(@impl $($left)* ; $first $($rest)*);
+        impl_tupleops!(@recur $($left)* $first ; $($rest)*);
+    };
+    ($($types:ident)*) => {
+        impl_tupleops!(@recur ; $($types)*);
+    };
+}
+
+macro_rules! tuple_impl {
+    ($low:literal, $high:literal) => {
+        // N - total tuple length
+        // This is N^2 so N shouldn't be too large.
+        seq!(N in $low..=$high {
+            #(
+                seq!(J in 0..N {
+                    impl<#(T~J,)*> seal::Sealed for (#(T~J,)*) {}
+                    impl<'a, #(T~J,)*> seal::Sealed for &'a (#(T~J,)*) {}
+
+                    impl_tupleops!(#(T~J)*);
+
+                    seq!(I in 0..N {
+                        // Index by value
+                        #[cfg_attr(not(feature = "impl_docs"), doc(hidden))]
+                        impl<#(T~J,)*> TupleIdx<I> for (#(T~J,)*) {
+                            type Output = T~I;
+                            const INDEX: usize = I;
+
+                            #[allow(non_snake_case, unused_variables)]
+                            fn idx(self) -> Self::Output {
+                                let (#(T~J,)*) = self;
+                                T~I
+                            }
+                        }
+
+                        // Index by reference
+                        #[cfg_attr(not(feature = "impl_docs"), doc(hidden))]
+                        impl<'a, #(T~J,)*> TupleIdx<I> for &'a (#(T~J,)*) {
+                            type Output = &'a T~I;
+                            const INDEX: usize = I;
+
+                            #[allow(non_snake_case, unused_variables)]
+                            fn idx(self) -> Self::Output {
+                                let (#(T~J,)*) = self;
+                                T~I
+                            }
+                        }
+                    });
+                });
+            )*
+        });
+    };
+}
+
+tuple_impl!(0, 16);
+#[cfg(any(feature = "tuple_32", feature = "tuple_24"))]
+tuple_impl!(17, 24);
+#[cfg(any(feature = "tuple_32"))]
+tuple_impl!(25, 32);
---
+++ src/test.rs
@@ -0,0 +1,72 @@
+#![allow(clippy::unit_cmp, clippy::just_underscores_and_digits)]
+
+use super::*;
+
+#[test]
+fn join() {
+    assert_eq!((1,).join((2,)), (1, 2));
+    assert_eq!((1, 'a',).join((2, 'b',)), (1, 'a', 2, 'b'));
+}
+
+#[test]
+fn join_ref() {
+    assert_eq!((&(1,)).join(&(2,)), (&1, &2));
+    assert_eq!((&(1, 'a',)).join(&(2, 'b',)), (&1, &'a', &2, &'b'));
+}
+
+#[test]
+fn join_nil() {
+    assert_eq!(().join(()), ());
+    assert_eq!((1,).join(()), (1,));
+    assert_eq!(().join((1,)), (1,));
+}
+
+#[test]
+fn split() {
+    let ((a, b), rest) = (1, 'a', 2, 'b').split();
+
+    assert_eq!(a, 1);
+    assert_eq!(b, 'a');
+    assert_eq!(rest, (2, 'b'));
+}
+
+#[test]
+fn split_ref() {
+    let ((a, b), rest) = (&(1, 'a', 2, 'b')).split();
+
+    assert_eq!(a, &1);
+    assert_eq!(b, &'a');
+    assert_eq!(rest, (&2, &'b'));
+}
+
+#[test]
+fn split_nil() {
+    let ((), ()) = ().split();
+    let ((_,), ()) = (1,).split();
+    let ((), (_,)) = (1,).split();
+}
+
+#[test]
+fn index() {
+    let a: &char = TupleIdx::<1>::idx(&(1, 'a'));
+
+    assert_eq!(*a, 'a');
+
+    assert_eq!(tuple_idx::<1, _>((1, 'a')), 'a');
+}
+
+#[test]
+fn boundaries() {
+    let seq!(N in 0..16 { (#(_~N,)*) }) =
+        seq!(I in 0..8 { (#(I,)*) }).join(seq!(J in 0..8 { (#(J,)*) }));
+    #[cfg(any(feature = "tuple_24", feature = "tuple_32"))]
+    {
+        let seq!(N in 0..24 { (#(_~N,)*) }) =
+            seq!(I in 0..12 { (#(I,)*) }).join(seq!(J in 0..12 { (#(J,)*) }));
+    }
+    #[cfg(any(feature = "tuple_32"))]
+    {
+        let seq!(N in 0..32 { (#(_~N,)*) }) =
+            seq!(I in 0..16 { (#(I,)*) }).join(seq!(J in 0..16 { (#(J,)*) }));
+    }
+}
